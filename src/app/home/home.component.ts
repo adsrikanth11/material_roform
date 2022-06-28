f80d3edd66a439b75d95931b27701c0623f2c7ac
adsrikanth11 <adsrikanth11@outlook.com>
2022-06-28 17:28:20 +0530

    Step 1 accordion issue fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,12 +14,12 @@ export class HomeComponent implements OnInit {
 
   roform: any = FormGroup;
   panelOpenState = true;
-  acc_condition: string = 'No';
-  myaccessories: string = '';
-  accessories = ['', 'case', 'battery', 'charger'];
-  case: boolean = false;
-  battery: boolean = false;
-  charger: boolean = false;
+  acc_conditions: string = 'No';
+  myaccessory: string = '';
+  // accessories = ['', 'case', 'battery', 'charger'];
+  // case: boolean = false;
+  // battery: boolean = false;
+  // charger: boolean = false;
   repair_type: string = 'Out of Warranty';
   logistic_provider: string = 'GLS';
   urls: any = [];
@@ -28,10 +28,10 @@ export class HomeComponent implements OnInit {
   fileAttr = 'Choose File';
   uploadFileEvt(imgFile: any) {
     if (imgFile.target.files && imgFile.target.files[0]) {
-      this.fileAttr = '';
-      Array.from(imgFile.target.files).forEach((file: any) => {
-        this.fileAttr += file.name + ' - ';
-      });
+      // this.fileAttr = '';
+      // Array.from(imgFile.target.files).forEach((file: any) => {
+      //   this.fileAttr += file.name + ' - ';
+      // });
       // HTML5 FileReader API
       let reader = new FileReader();
       reader.onload = (e: any) => {
@@ -46,7 +46,7 @@ export class HomeComponent implements OnInit {
       // Reset if duplicate image uploaded again
       this.fileInput.nativeElement.value = '';
     } else {
-      this.fileAttr = 'Choose File';
+      // this.fileAttr = 'Choose File';
     }
   }
   
@@ -60,7 +60,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.roform = this.fb.group({
-      title: ['', Validators.required],
+      title: [''],
       first_last_name: [''],
       mobile_number: [],
       email_address: [''],
@@ -76,64 +76,33 @@ export class HomeComponent implements OnInit {
       dpd_pickup_date: [],
       terms_conditions: [],
       data_protection_notice: [],
-      products: new FormArray([
+      products: this.fb.array([
         this.fb.group({
-          'tool_type_number': new FormControl('', Validators.required),
-          'tool_name': new FormControl(''),
-          'serial_number': new FormControl(''),
-          'description': new FormControl(''),
-          'acc_condition': new FormControl(''),
-          'myaccessories': new FormArray([
+          'tool_type_number': [''],
+          'tool_name': [''],
+          'serial_number': [''],
+          'description': [''],
+          'acc_condition': [''],
+          'myaccessories': this.fb.array([
             this.fb.group({
-              'Case': new FormControl(''),
-              'case_quantity': new FormControl('')
+              'case': '',
+              'case_quantity': ''
             }),
             this.fb.group({
-              'Battery': new FormControl(''),
-              'battery_quantity': new FormControl('')
+              'battery': '',
+              'battery_quantity': ''
             }),
             this.fb.group({
-              'Charger': new FormControl(''),
-              'charger_quantity': new FormControl('')
+              'charger': '',
+              'charger_quantity': ''
             }),
           ]),
-          'repair_type': new FormControl(''),
-          'cost_limit': new FormControl('')
+          'repair_type': [''],
+          'cost_limit': [''],
+          'warranty_files': []
         })
       ])
     });
-    console.log(this.roform.value);
-  }
-
-  uploadfile(event: any) {
-    const files = event.target.files;
-    this.fileAttr = '';
-      Array.from(event.target.files).forEach((file: any) => {
-        this.fileAttr += file.name + ' - ';
-      });
-    for (let index = 0; index < files.length; index++) {
-      const element = files[index];
-      console.log(element.name);
-
-      let reader = new FileReader();
-
-      reader.readAsDataURL(files[index]);
-
-      reader.onload = (e: any) => {
-        let image = new Image();
-        image.src = e.target.result;
-        image.onload = (rs) => {
-          let imgBase64Path = e.target.result;
-        };
-        this.urls.push(e.target.result);
-        console.log(this.urls);
-      }
-    }
-  }
-
-  remove_img(url: any) {
-    console.log(url);
-    this.urls.splice(url);
   }
 
   add_product() {
@@ -146,15 +115,15 @@ export class HomeComponent implements OnInit {
       'acc_condition': '',
       'myaccessories': [
         {
-          'Case': '',
+          'case': '',
           'case_quantity': ''
         },
         {
-          'Battery': '',
+          'battery': '',
           'battery_quantity': ''
         },
         {
-          'Charger': '',
+          'charger': '',
           'charger_quantity': ''
         },
       ],
@@ -167,6 +136,36 @@ export class HomeComponent implements OnInit {
   del_product(i: any) {
     let products_array = this.roform.get('products') as FormArray;
     products_array.removeAt(i);
+  }
+
+  uploadfile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const files = event.target.files;
+      this.fileAttr = '';
+      Array.from(event.target.files).forEach((file: any) => {
+        this.fileAttr += file.name;
+        let products_array = this.roform.products.get('warranty_files') as FormArray;
+        products_array.push(file.name);
+      });
+      for (let index = 0; index < files.length; index++) {
+        const element = files[index];
+        // console.log(element.name);
+        // this.fileAttr += element.name;
+        let reader = new FileReader();
+
+        reader.readAsDataURL(files[index]);
+
+        reader.onload = (e: any) => {
+          this.urls.push(e.target.result);
+        }
+      }
+    } else {
+      this.fileAttr = 'Choose file';
+    }
+  }
+
+  remove_img(url: any) {
+    this.urls.splice(url);
   }
 
   Submit_Roform() {
