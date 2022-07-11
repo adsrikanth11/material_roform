2f3ad4e8ea0533805c86eb8732401fd2e5e9e78b
adsrikanth11 <adsrikanth11@outlook.com>
2022-07-11 23:43:58 +0530

    my first roform init

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -90,24 +90,30 @@ export class HomeComponent implements OnInit {
       let file_type = event.target.files[0].type;
       // Check file type
       if(file_type === 'image/jpeg' || file_type === 'image/png' || file_type === 'image/gif' || file_type === 'application/pdf') {
-        Array.from(event.target.files).forEach((file: any) => {
-          // Upload images
-          this.upload_img.push(file.name);
-          this.file_name.push(file.name);
-          this.files_length = this.upload_img.length;
-          this.roform.patchValue({
-            products: [{
-              warranty_files: this.upload_img
-            }]
-          });
+        Array.from(event.target.files).forEach((file: any, index: any) => {
+          // Upload file limit
+          if(index <= 2) {
+            this.upload_img.push(file.name);
+            this.file_name.push(file.name);
+            this.files_length = this.upload_img.length;
+            // Upload files
+            this.roform.patchValue({
+              products: [{
+                warranty_files: this.upload_img
+              }]
+            });
+          } 
         });
-        // Preview images
         for (let i = 0; i < event.target.files.length; i++) {
           var reader = new FileReader();
-          reader.onload = (event:any) => {
-            this.preview_img.push(event.target.result);
+          // Preview file limit
+          if(i <= 2) {
+            // Preview files
+            reader.onload = (event:any) => {
+              this.preview_img.push(event.target.result);
+            }
+            reader.readAsDataURL(event.target.files[i]);
           }
-          reader.readAsDataURL(event.target.files[i]);
         }
       } else {
         alert('upload only jpg, png, gif, pdf only');
@@ -117,15 +123,29 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  del_img(img: any) {
+  del_img(imgs: any, img: any, index: any) {
     this.preview_img.splice(img, 1);
-    this.roform.patchValue({
-      products: [{
-        warranty_files: this.upload_img
-      }]
-    });
-    this.roform.controls.products.controls.warranty_files.splice(img, 1);
-    console.log(this.roform.value.products);
+    let myindex = this.upload_img.indexOf(img);
+    if (myindex !== -1) {
+      this.upload_img.splice(myindex, 1);
+      this.file_name.splice(myindex, 1);
+      this.roform.patchValue({
+        products: [{
+          warranty_files: this.upload_img
+        }]
+      });
+    }
+    this.files_length = this.roform.value.products[0].warranty_files.length;
+    // console.log(this.roform.value.products[0].warranty_files.length);
+    // this.upload_img.splice(img);
+    // this.files_length = this.upload_img.length;
+    // console.log("images: " + imgs);
+    // console.log("image: " + img + " index: " + index);
+    // console.log("upload_img_: " + this.upload_img);
+    // console.log("files_length: " + this.files_length);
+    
+    // this.roform.controls.products.controls.warranty_files.splice(img, 1);
+    // console.log(this.roform.value.products);
   }
 
   add_product() {
